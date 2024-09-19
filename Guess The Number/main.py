ec10b1ec81e01bb5e0bbad65f48e1560acf1a40d
Rounak <150204832+rounaksarak@users.noreply.github.com>
2024-09-19 10:54:27 +0530

    adding new game

---
+++ Guess The Number/main.py
@@ -0,0 +1,35 @@
+import random
+import time
+import random
+print("Hey, I have chosen a number between 1 and 100 and you have to find it.")
+print("Write any number to start. And I will start the timer from there.")
+randomNum = 0
+startTime = 0
+
+while True:
+    if not randomNum:
+        randomNum = int(random.random() * 100)
+
+    userNum = int(input("I guess the number is : "))
+
+    if not startTime:
+        startTime = time.time()
+
+    if userNum == randomNum:
+        print("Hey you caught it. ")
+        print("The time taken is just ",int(time.time() - startTime),"sec")
+        user = input("Want to play again (Enter y to continue) : ")
+        if user == "y" or user == "Y":
+            randomNum = 0
+            startTime = 0
+            continue
+        else:
+            break
+    elif userNum > randomNum:
+
+        print("Your number is larger than mine. Try smaller number.")
+        print("Time used : ",int(time.time() - startTime),"sec")
+    else:
+        print("Your number is smaller than mine. Try larger number.")
+        print("Time used : ", int(time.time() - startTime), "sec")
+
