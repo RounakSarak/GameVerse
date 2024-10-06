3e119b8076a600f38ad7fa723f7adcd92b6d6a76
PC & Hardware <pcandhardware55@gmail.com>
2024-10-06 13:53:40 +0530

    Fixed the biggest logical error during gameplay

--- High Low/main.py
+++ High Low/main.py
@@ -1,16 +1,26 @@
 from random import randint
 from time import sleep
 import json
-
+from os import path
+from math import ceil
 def get_score() -> tuple:
-    with open("status.json") as file:
+    if not path.exists(r"High Low\status.json"):
+        data = {
+            "win": 0,
+            "fail": 0,
+            "plays": 0 
+        }
+        json.dump(data,open(r"High Low\status.json","x"))
+    with open(r"High Low\status.json") as file:
         status = json.load(file)
     win = status["win"]
     fail = status["fail"]
     plays = status["plays"]
 
-    win = win*100//plays
-    fail = fail*100//plays
+    if plays == 0:
+        return 0,0,0
+    win = ceil(win*100/plays)
+    fail = ceil(fail*100/plays)
     return win,fail,plays
 
 print("Start the System?(y/n)")
@@ -28,7 +38,7 @@ print("I selected 2 random numbers\nYou have to guess whether the first number i
 
 def add_score(add: bool) -> None:
     
-    with open("status.json") as file:
+    with open(r"High Low\status.json") as file:
         status = json.load(file)
     win = status["win"]
     fail = status["fail"]
@@ -40,18 +50,18 @@ def add_score(add: bool) -> None:
             "fail":fail,
             "plays":plays+1 
         }
-        json.dump(data,open("status.json","wt"))
+        json.dump(data,open(r"High Low\status.json","wt"))
     else:
         data = {
             "win": win,
             "fail":fail+1,
             "plays":plays+1 
         }
-        json.dump(data,open("status.json","wt"))
+        json.dump(data,open(r"High Low\status.json","wt"))
 
 while True:
     target = randint(1,100)
-    via = randint(1,100)
+    via = randint(20,85)
     sleep(0.3)
 
     print(f"so tell me\nIs {via} high or low than the target number?")
@@ -61,7 +71,7 @@ while True:
         except ValueError:
             print("Just enter h/l/bingo")
 
-        if user == "h":
+        if user == "l":
             if via < target:
                 print(f"Noice it was {via} and {target}")
                 add_score(True)
@@ -70,7 +80,7 @@ while True:
                 print(f"You lost! it was {via} and {target}")
                 add_score(False)
                 break
-        elif user == "l":
+        elif user == "h":
             if via > target:
                 print(f"Noice it was {via} and {target}")
                 add_score(True)
